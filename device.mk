ef8cd32871a811096b3c90590cd818da3a1492ba
McFy49 <31405855+McFy49@users.noreply.github.com>
2018-01-13 11:52:34 +0100

    fixed right soc

--- device.mk
+++ device.mk
@@ -82,7 +82,7 @@ ifeq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_PROPERTY_OVERRIDES += \
        ro.secure=0 \
        ro.adb.secure=0 \
-       ro.hardware=universal7580 \
+       ro.hardware=universal7870 \
        persist.service.adb.enable=1 \
        persist.service.debuggable=1 \
        persist.sys.usb.config=adb \
